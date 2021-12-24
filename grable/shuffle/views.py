a8cb4df4344b5e92451092eb89293f6b20755fca
Kebron <Kebronjrdaniel@gmail.com>
2021-12-24 15:03:27 +0300

    Shuffle button does it's own task

--- grable/shuffle/views.py
+++ grable/shuffle/views.py
@@ -22,25 +22,24 @@ def shuffler(request):
     for user in users:
         members.append(user.username)
 
-    # random.shuffle(members)
-    print(members)
+    random.shuffle(members)
+    all = ",".join(members)
+    print(all)
     chunks = [members[x : x + 3] for x in range(0, len(members), 3)]
+
+    if request.method == "POST" and "shuffle" in request.POST:
+        print("Shuffle Clicked")
+        return redirect("/")
 
     form = RoundForm()
     if request.method == "POST":
         name = request.POST["name"]
-        mem = ",".join(members)
-        print(mem)
-        sys = request.POST.get("test")
-        print(sys)
-        # round = Round(name=name, members=members)
-        # round.save()
+        print(name)
+        print(all)
         return redirect("/")
 
     return render(
-        request,
-        "shuffle/shuffler.html",
-        context={"members": members, "chunks": chunks, "form": form},
+        request, "shuffle/shuffler.html", context={"chunks": chunks, "form": form}
     )
 
 
