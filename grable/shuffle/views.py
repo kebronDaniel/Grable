8b907735839cea6ee356febe038a285748b64794
Kebron <Kebronjrdaniel@gmail.com>
2021-12-28 11:41:57 +0300

    Consistent first and second round

--- grable/shuffle/views.py
+++ grable/shuffle/views.py
@@ -1,6 +1,7 @@
 from collections import abc
 from django import forms
 from django.db import router
+from django.forms.widgets import RadioSelect
 from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -22,22 +23,15 @@ def shuffler(request):
     for user in users:
         members.append(user.username)
 
-    random.shuffle(members)
-    all = ",".join(members)
-    print(all)
     chunks = [members[x : x + 3] for x in range(0, len(members), 3)]
-
-    if request.method == "POST" and "shuffle" in request.POST:
-        print("Shuffle Clicked")
-        return redirect("/")
-
     form = RoundForm()
     if request.method == "POST":
+        random.shuffle(members)
+        print("First one")
+        print(members)
         name = request.POST["name"]
-        print(name)
-        print(all)
-        return redirect("/")
-
+        print("Second one")
+        print(members)
     return render(
         request, "shuffle/shuffler.html", context={"chunks": chunks, "form": form}
     )
